70edc1bcc5c74040cbe79f7b1b145f4e0aa8cf95
Killian Davitt <davittk@tcd.ie>
2016-04-12 15:47:40 +0100

    Added Haskell 2015 Q1

--- haskell/answers/2015-paper/2015-q1.hs
+++ haskell/answers/2015-paper/2015-q1.hs
@@ -4,20 +4,28 @@
 my_repeat :: a -> [a]
 my_repeat x = x:my_repeat x
 
+
 my_replicate :: Int -> a -> [a]
+my_replicate 0 _ = []
 my_replicate 1 y = [y]
 my_replicate x y = y:my_replicate (x-1) y
 
+
 my_concat :: [[a]] -> [a]
+my_concat [] = []
 my_concat [[a]] = [a]
 my_concat ([a]:as) = a : my_concat as
 
+
 my_zip :: [a] -> [b] -> [(a,b)]
+my_zip [] [] = []
+my_zip [] (a:as) = error "Lists are not same size"
+my_zip (a:as) [] = error "Lists are not same size"
 my_zip [a] [b] = [(a,b)]
 my_zip (a:as) (b:bs) = (a,b) : my_zip as bs
 
-my_unzip :: [(a,b)] -> ([a], [b])
 
+my_unzip :: [(a,b)] -> ([a], [b])
 my_unzip [] = ([], [])
 my_unzip [(x,y)] = ([x],[y])
 my_unzip ((x,y):qs) = (x:xs, y:ys)
