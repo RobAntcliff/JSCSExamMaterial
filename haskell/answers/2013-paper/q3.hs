415290ce49c64ceddd4baa9b49dba9059cc3be8f
CiaranCostello <costelci@tcd.ie>
2016-04-22 00:14:34 +0100

    Editted to use do notation for handling maybe monad and added Dvd.
    Signed-off-by: CiaranCostello <costelci@tcd.ie>

--- haskell/answers/2013-paper/q3.hs
+++ haskell/answers/2013-paper/q3.hs
@@ -14,16 +14,23 @@ lkp key ((tkey,tval):rest) =
     if key == tkey
         then Just tval
         else lkp key rest
+divBy :: Int -> Int -> Maybe Int
+divBy a b 
+    | b == 0 = Nothing
+    | otherwise = Just (a `div` b)
 
 eval :: Dict -> Expr -> Maybe Int
 eval _ (K i) = Just i
 eval d (V s) = lkp s d
-eval d (Add e1 e2) = case a of 
-        (Just a') -> case b of 
-            (Just b') -> Just (a' + b')
-            (Nothing) -> Nothing
-        (Nothing) -> Nothing
-    where (a, b) = (eval d e1, eval d e2)
+eval d (Add e1 e2) = do
+    a <- eval d e1
+    b <- eval d e2
+    return (a + b)
+eval d (Dvd e1 e2) = do
+    a <- eval d e1
+    b <- eval d e2
+    c <- divBy a b
+    return c
 eval d (Let v e1 e2) = case i of 
        (Just a) -> eval (ins v a d) e2
        Nothing -> Nothing
